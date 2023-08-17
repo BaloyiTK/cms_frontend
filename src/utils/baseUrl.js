b92af489b3fbe3756d6300a119bd2be0cb62f233
Tiyani Baloyi <tiyanikevinbaloyi@gmail.com>
2023-08-17 11:31:06 +0200

    changed logo

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,2 +1,2 @@
-//export const baseUrl = process.env.REACT_APP_API_BASE_URL;
-export const baseUrl = 'http://localhost:3001/api';
+export const baseUrl = process.env.REACT_APP_API_BASE_URL;
+//export const baseUrl = 'http://localhost:3001/api';
