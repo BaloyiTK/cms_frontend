feef4095fc23687751ccb355939851c7afbb0889
BaloyiTK <tiyanikevinbaloyi@gmail.com>
2025-02-03 01:32:09 +0200

    azure deploymenr

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,3 +1,5 @@
 
-export const baseUrl = process.env.REACT_APP_API_BASE_URL;
-//export const baseUrl = 'http://localhost:3001/api'
+//export const baseUrl = process.env.REACT_APP_API_BASE_URL;
+//export const baseUrl = 'http://localhost:3001/api' 
+
+export const baseUrl = 'https://xpandcms.azurewebsites.net/api';
