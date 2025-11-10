79701255cd1b47949cdce87b486eafe4229e6378
tiyani-baloyi <tiyani@ikusasatech.com>
2025-11-10 12:11:32 +0200

    initial commit

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -2,4 +2,6 @@
 //export const baseUrl = process.env.REACT_APP_API_BASE_URL;
 //export const baseUrl = 'http://localhost:3001/api' 
 
-export const baseUrl = 'http://173.249.32.152:3001/api';
+export const baseUrl = 'http://localhost:3001/api';
+
+//export const baseUrl = 'http://173.249.32.152:3001/api';
