d591f7bf85a4756f3b4467845e7bfcf05ed06320
Tiyani Kevin Baloyi <114812641+BaloyiTK@users.noreply.github.com>
2025-11-10 11:04:13 +0200

    Update baseUrl.js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -2,4 +2,4 @@
 //export const baseUrl = process.env.REACT_APP_API_BASE_URL;
 //export const baseUrl = 'http://localhost:3001/api' 
 
-export const baseUrl = 'https://xpandcms.azurewebsites.net/api';
+export const baseUrl = 'http://173.249.32.152:3001/api';
